2178b47dd15af251efd304bf3fe9b18c504653f9
codeanticode <andres.colubri@gmail.com>
2020-07-07 08:55:43 +0900

    latest pdf

---
+++ paper/latex/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm arxiv.tar.gz
+rm ilmrisk.aux
+rm ilmrisk.bbl
+rm ilmrisk.bcf
+rm ilmrisk.blg
+rm ilmrisk.fdb_latexmk
+rm ilmrisk.fls
+rm ilmrisk.log
+rm ilmrisk.pdf
+rm ilmrisk.run.xml
